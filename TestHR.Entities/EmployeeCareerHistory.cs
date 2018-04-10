6ac9bbb2f2be1fd7f8919132da630dcc075df78a
Rishad Wahid <rbwahid@gmail.com>
2018-04-10 18:04:35 +0600

    added Employee career and education. updated LeaveType

---
+++ TestHR.Entities/EmployeeCareerHistory.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TestHR.Data;
+
+namespace TestHR.Entities
+{
+    public class EmployeeCareerHistory : Entity
+    {
+        public string CompanyName { get; set; }
+        public string CompanyAddress { get; set; }
+        public string Department { get; set; }
+        public string Position { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+    }
+}
---
+++ TestHR.Entities/LeaveType.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TestHR.Data;
+
+namespace TestHR.Entities
+{
+    public class LeaveType : Entity
+    {
+        public string Name { get; set; }
+        public int NumberOfDays { get; set; }
+    }
+}
