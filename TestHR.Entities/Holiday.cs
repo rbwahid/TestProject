2945227dc23a22b532e3ce5a81fc2fc3caef52b6
Rishad Wahid <rbwahid@gmail.com>
2018-04-10 21:25:11 +0600

    wip holiday, shift.

---
+++ TestHR.Entities/Holiday.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TestHR.Data;
+
+namespace TestHR.Entities
+{
+    public class Holiday : Entity
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public DateTime DateFrom { get; set; }
+        public DateTime DateTo { get; set; }
+    }
+}
---
+++ TestHR.Entities/Shift.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TestHR.Data;
+
+namespace TestHR.Entities
+{
+    public class Shift : Entity
+    {
+        public string Name { get; set; }
+        public string Code { get; set; }
+        public string Type { get; set; }
+        public string Description { get; set; }
+        public bool IsDefault { get; set; }
+        public bool IsActive { get; set; }
+        public string OfficeHourDescription { get; set; }
+    }
+}
