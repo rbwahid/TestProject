6db4d2d384153e7d4f25a6b3323d68b2cefc1822
Tawhidul <tawhidul@encodersbd.com>
2018-04-10 12:26:56 +0600

    wip

---
+++ TestHR.AdminCenter/BranchManagementService.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TestHR.Entities;
+
+namespace TestHR.AdminCenter
+{
+    public class BranchManagementService
+    {
+        private AdminCenterDbContext _context;
+        private BranchUnitOfWork _branchUnitOfWork;
+        private CompanyUnitOfWork _companyUnitOfWork;
+
+        public BranchManagementService()
+        {
+            _context = new AdminCenterDbContext();
+            _branchUnitOfWork = new BranchUnitOfWork(_context);
+            _companyUnitOfWork = new CompanyUnitOfWork(_context);
+        }
+
+        public List<Branch> GetAllBranches()
+        {
+            return _branchUnitOfWork.BranchRepository.GetAll().ToList();
+        }
+
+        public Branch GetBranch(Guid id)
+        {
+            return _branchUnitOfWork.BranchRepository.GetById(id);
+        }
+
+         public void DeleteBranch(Guid id)
+        {
+            _branchUnitOfWork.BranchRepository.DeleteById(id);
+            _branchUnitOfWork.Save();
+        }
+
+        public int GetCount()
+        {
+
+            return _branchUnitOfWork.BranchRepository.GetAll().Count();
+            
+        }
+        public void AddBranch(string name, Guid CompanyId, string Description)
+        {
+            var branch = new Branch();
+
+            branch.Name = name;
+            branch.Company = _companyUnitOfWork.CompanyRepository.GetById(CompanyId);
+            branch.Description = Description;
+
+            _branchUnitOfWork.BranchRepository.Add(branch);
+            _branchUnitOfWork.Save();
+
+        }
+    }
+}
---
+++ TestHR.Web/Areas/Admin/Controllers/BranchController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using TestHR.Web.Areas.Admin.Models;
+
+namespace TestHR.Web.Areas.Admin.Controllers
+{
+    public class BranchController : Controller
+    {
+        //
+        // GET: /Admin/Branch/
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Add()
+        {
+            var branchModel = new BranchModel();
+            return View(branchModel);
+        }
+        [HttpPost]
+        public ActionResult Add(BranchModel branchModel)
+        {
+            //try
+            //{
+                branchModel.AddBranch();
+                TempData["message"] = "Successfully added Branch.";
+                TempData["alertType"] = "success";
+            //}
+
+            //catch (Exception e)
+            //{
+            //    TempData["message"] = "Failed to Add Branch.";
+            //    TempData["alertType"] = "danger";
+            //    Console.Write(e.Message);
+            //}
+
+            return View(branchModel);
+        }
+	}
+}
