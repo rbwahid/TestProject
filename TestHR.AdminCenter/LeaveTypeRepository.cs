b9138e1058a7ff485a5873fe98d6da4f98031592
sahadatHossen <sahadat@encodersbd.com>
2018-04-12 13:26:53 +0600

    Leave Type Adding Done

---
+++ TestHR.AdminCenter/LeaveTypeRepository.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TestHR.Data;
+using TestHR.Entities;
+
+namespace TestHR.AdminCenter
+{
+    public class LeaveTypeRepository:Repository<LeaveType>
+    {
+        private AdminCenterDbContext _context;
+
+        public LeaveTypeRepository(AdminCenterDbContext context):base(context)
+        {
+            _context = context;
+        }
+    }
+}
