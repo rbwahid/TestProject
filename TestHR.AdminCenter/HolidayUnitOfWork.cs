3df47a92a8b51d7df6a0e359e98f35ba8276d931
Tawhidul <tawhidul@encodersbd.com>
2018-04-12 19:48:35 +0600

    Add Holiday

---
+++ TestHR.AdminCenter/HolidayUnitOfWork.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestHR.AdminCenter
+{
+    class HolidayUnitOfWork
+    {
+        private AdminCenterDbContext _context { get; set; }
+        private HolidayRepository _holidayRepository { get; set; }
+
+        public HolidayUnitOfWork(AdminCenterDbContext context)
+        {
+            _context = context;
+            _holidayRepository = new HolidayRepository(_context);
+        }
+
+        public HolidayRepository HolidayRepository
+        {
+            get
+            {
+                return _holidayRepository;
+            }
+        }
+        public void Save()
+        {
+            _context.SaveChanges();
+        }
+
+        public void Dispose()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
