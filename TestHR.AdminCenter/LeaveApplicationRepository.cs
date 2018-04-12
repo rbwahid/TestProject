9540dba01088efe3abebbd85f3da4316b3ab7b4c
sahadatHossen <sahadat@encodersbd.com>
2018-04-12 20:42:49 +0600

    Leave Application Added

---
+++ TestHR.AdminCenter/LeaveApplicationRepository.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TestHR.Data;
+using TestHR.Entities;
+
+namespace TestHR.AdminCenter
+{
+  public class LeaveApplicationRepository:Repository<LeaveApplication>
+    {
+       private AdminCenterDbContext _context;
+       public LeaveApplicationRepository(AdminCenterDbContext context)
+            :base(context)
+        {
+            _context = context;
+        }
+    }
+}
---
+++ TestHR.Web/Areas/Admin/Controllers/LeaveApplicationController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using TestHR.Web.Areas.Admin.Models;
+
+namespace TestHR.Web.Areas.Admin.Controllers
+{
+    public class LeaveApplicationController : Controller
+    {
+        //
+        // GET: /Admin/LeaveApplication/
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Add()
+        {
+            LeaveApplicationModel model = new LeaveApplicationModel();
+
+            return View(model);
+        }
+        [HttpPost]
+        public ActionResult Add(LeaveApplicationModel leaveApplicationModel)
+        {
+
+            leaveApplicationModel.AddLeaveApplication();
+            TempData["message"] = "Successfully added Branch.";
+            TempData["alertType"] = "success";
+            return View(leaveApplicationModel);
+        }
+	}
+}
