bccc5134cb9330ea25b695cda4b536b9bdc7989f
Tawhidul <tawhidul@encodersbd.com>
2018-04-15 16:56:27 +0600

    Add Shift

---
+++ TestHR.AdminCenter/ShiftManagementService.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TestHR.Entities;
+
+namespace TestHR.AdminCenter
+{
+    public class ShiftManagementService
+    {
+        private AdminCenterDbContext _context;
+        private ShiftUnitOfWork _shifthUnitOfWork;
+        private BranchUnitOfWork _branchUnitOfWork;
+        private CompanyUnitOfWork _companyUnitOfWork;
+
+        public ShiftManagementService()
+        {
+            _context = new AdminCenterDbContext();
+            _shifthUnitOfWork = new ShiftUnitOfWork(_context);
+            _branchUnitOfWork = new BranchUnitOfWork(_context);
+            _companyUnitOfWork = new CompanyUnitOfWork(_context);
+        }
+
+        public List<Shift> GetAllShifts()
+        {
+            return _shifthUnitOfWork.ShiftRepository.GetAll().ToList();
+        }
+
+        public Shift GetShift(Guid id)
+        {
+            return _shifthUnitOfWork.ShiftRepository.GetById(id);
+        }
+
+         public void DeleteShift(Guid id)
+        {
+            _shifthUnitOfWork.ShiftRepository.DeleteById(id);
+            _shifthUnitOfWork.Save();
+        }
+
+        public int GetCount()
+        {
+
+            return _shifthUnitOfWork.ShiftRepository.GetAll().Count();
+            
+        }
+        public void AddShift(string Name, string Code, string Type, string Description, bool IsDefault, bool IsActive, string OfficeHourDescription)
+        {
+            var shift = new Shift();
+
+            shift.Name = Name;
+            shift.Code = Code;
+            shift.Type = Type;
+            shift.Description = Description;
+            shift.IsDefault = IsDefault;
+            shift.IsActive = IsActive;
+            shift.OfficeHourDescription = OfficeHourDescription;
+            _shifthUnitOfWork.ShiftRepository.Add(shift);
+            _shifthUnitOfWork.Save();
+
+        }
+    }
+}
---
+++ TestHR.Web/Areas/Admin/Models/ShiftModel.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using TestHR.AdminCenter;
+using TestHR.Entities;
+
+namespace TestHR.Web.Areas.Admin.Models
+{
+    public class ShiftModel
+    {
+        private ShiftManagementService _shiftManagementService { get; set; }
+        private CompanyManagementService _companyManagementService { get; set; }
+        private BranchManagementService _branchManagementService { get; set; }
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string Code { get; set; }
+        public string Type { get; set; }
+        public string Description { get; set; }
+        public bool IsDefault { get; set; }
+        public bool IsActive { get; set; }
+        public string OfficeHourDescription { get; set; }
+        public ShiftModel()
+        {
+            _companyManagementService = new CompanyManagementService();
+            _branchManagementService = new BranchManagementService();
+            _shiftManagementService =new ShiftManagementService();
+        }
+        public List<Company> GetAllCompanies()
+        {
+           
+            return _companyManagementService.GetAllCompanies();
+        }
+
+        public ShiftModel(Guid id)
+            : this()
+        {
+            var shift = _shiftManagementService.GetShift(id);
+
+            this.Id = shift.Id;
+            this.Name = shift.Name;
+            this.Code = shift.Code;
+            this.Type = shift.Type;
+            this.Description = shift.Description;
+            this.IsDefault = shift.IsDefault;
+            this.IsActive = shift.IsActive;
+            this.OfficeHourDescription = shift.OfficeHourDescription;
+        }
+        public void AddShift()
+        {
+            _shiftManagementService.AddShift( Name, Code, Type, Description, IsDefault, IsActive,OfficeHourDescription);
+        }
+
+        internal void DeleteShift(Guid? id)
+        {
+            if (id.HasValue)
+            {
+                _shiftManagementService.DeleteShift(id.Value);
+            }                
+            else
+            {
+                throw new Exception();
+            }
+                
+        }
+
+        public Shift LoadShiftData(Guid? id)
+        {
+            if (id.HasValue)
+            {
+                return _shiftManagementService.GetShift(id.Value);
+            }
+            else
+            {
+                throw new Exception();
+            }
+                
+        }
+    }
+}
