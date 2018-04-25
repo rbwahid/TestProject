25e1944d8cdf1c15f316012fc35a52b935452543
Tawhidul <tawhidul@encodersbd.com>
2018-04-25 17:01:14 +0600

    View_Calender

---
+++ TestHR.AdminCenter/CalenderUnitOfWork.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestHR.AdminCenter
+{
+    class CalenderUnitOfWork : IDisposable
+    {
+        private AdminCenterDbContext _context { get; set; }
+        private CalenderRepository _calenderRepository { get; set; }
+
+        public CalenderUnitOfWork(AdminCenterDbContext context)
+        {
+            _context = context;
+            _calenderRepository = new CalenderRepository(_context);
+        }
+
+        public CalenderRepository CalenderRepository
+        {
+            get
+            {
+                return _calenderRepository;
+            }
+        }
+        public void Save()
+        {
+            _context.SaveChanges();
+        }
+
+        public void Dispose()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ TestHR.Web/Areas/Admin/Models/CalenderModel.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+using TestHR.AdminCenter;
+using TestHR.Entities;
+
+namespace TestHR.Web.Areas.Admin.Models
+{
+    public class CalenderModel
+    {
+        private CalenderManagementService _calenderManagementService { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public DateTime DateFrom { get; set; }
+        public DateTime DateTo { get; set; }
+
+        public CalenderModel()
+        { 
+            _calenderManagementService = new CalenderManagementService();
+        }
+        public List<Holiday> GetAllHolidays()
+        {
+           
+            return _calenderManagementService.GetAllHolidays();
+        }
+
+       
+        public void AddToCalender()
+        {
+            _calenderManagementService.AddToCalender(Name, Description, DateFrom, DateTo);
+        }
+       
+        internal void DeleteHoliday(Guid? id)
+        {
+            if (id.HasValue)
+            {
+                _calenderManagementService.DeleteHoliday(id.Value);
+            }                
+            else
+            {
+                throw new Exception();
+            }
+                
+        }
+
+        public Holiday LoadCalender(Guid? id)
+        {
+            if (id.HasValue)
+            {
+                return _calenderManagementService.GetCalender(id.Value);
+            }
+            else
+            {
+                throw new Exception();
+            }
+                
+        }
+    }
+    
+}
