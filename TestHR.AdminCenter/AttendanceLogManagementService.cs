f7fe7f9d60da3fa31b64bc5ceaf83d5bf265867b
Tawhidul <tawhidul@encodersbd.com>
2018-05-09 14:56:31 +0600

    Import Attendance Log Excel File

---
+++ TestHR.AdminCenter/AttendanceLogManagementService.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TestHR.Entities;
+
+
+namespace TestHR.AdminCenter
+{
+    public class AttendanceLogManagementService
+    {
+        private AdminCenterDbContext _context;
+        private AttendanceLogUnitOfWork _attendanceLogUnitOfWork;
+
+        public AttendanceLogManagementService()
+        {
+            _context = new AdminCenterDbContext();
+            _attendanceLogUnitOfWork = new AttendanceLogUnitOfWork(_context);
+        }
+
+        public List<AttendanceLog> GetAllAttendanceLog()
+        {
+            return _attendanceLogUnitOfWork.AttendanceLogRepository.GetAll().ToList();
+        }
+
+        public AttendanceLog GetAttendanceLog(Guid id)
+        {
+            return _attendanceLogUnitOfWork.AttendanceLogRepository.GetById(id);
+        }
+         public void DeleteAttendanceLog(Guid id)
+        {
+            _attendanceLogUnitOfWork.AttendanceLogRepository.DeleteById(id);
+            _attendanceLogUnitOfWork.Save();
+        }
+
+        public int GetCount()
+        {
+
+            return _attendanceLogUnitOfWork.AttendanceLogRepository.GetAll().Count();
+            
+        }
+        public void AddToAttendanceLog(string employeeName, DateTime attendanceDate, TimeSpan PunchTime)
+        {
+            var attendanceInfo = new AttendanceLog();
+            attendanceInfo.Employee=new Employee();
+            attendanceInfo.Employee.FirstName = employeeName;
+            attendanceInfo.AttendanceDate = attendanceDate;
+            attendanceInfo.PunchTime = PunchTime;
+            _attendanceLogUnitOfWork.AttendanceLogRepository.Add(attendanceInfo);
+            _attendanceLogUnitOfWork.Save();
+
+        }
+    }
+}
---
+++ TestHR.Web/Areas/Admin/Controllers/AttendanceController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using OfficeOpenXml;
+using TestHR.Entities;
+using TestHR.Web.Areas.Admin.Models;
+
+namespace TestHR.Web.Areas.Admin.Controllers
+{
+    public class AttendanceController : Controller
+    {
+        //
+        // GET: /Admin/Attendance/
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult AttendanceLogFileImport()
+        {
+            return View("AttendanceLogFileImport");
+        }
+        [HttpPost]
+        public ActionResult AttendanceLogFileImport(HttpPostedFileBase attendanceLogExcelFile)
+        {
+            if (ModelState.IsValid)
+            {
+                AttendanceLogModel attendanceLogModel=new AttendanceLogModel();
+                attendanceLogModel.AttendanceLogFileImport(attendanceLogExcelFile);
+            }
+            return View();
+        }
+	}
+}
